7de17bfd8babafce30a5e0068dbdedd78116ab18
Keith Kyzivat <keith.kyzivat@qt.io>
2023-09-01 17:22:12 -0400

    Switch to example.com for testing
    Google thinks that my traffic is unusual (it is) and are starting to
    consider me a malicious actor of some sort - having me prove I am human
    before showing the google page, so, before anything horrid happens, I am
    switching to a different test page.

--- main.cpp
+++ main.cpp
@@ -62,7 +62,8 @@ int main(int argc, char * argv[])
     if (argc > 1)
         url = QUrl::fromUserInput(argv[1]);
     else
-        url = QUrl("http://www.google.com/ncr");
+        url = QUrl("http://www.example.com/");
+//        url = QUrl("http://www.google.com/ncr");
     MainWindow *browser = new MainWindow(url);
     browser->resize(1024, 768);
     browser->show();
